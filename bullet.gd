99e6debf8fcc3ab412a90e1d5159608670ed1a75
Lazt Dev <fisabreyt123@gmail.com>
2026-02-05 12:55:28 +0100

    Next task :
    fixing enemy dpop when shooting

--- bullet.gd
+++ bullet.gd
@@ -8,6 +8,8 @@ func _ready():
 	global_position=pos
 	global_rotation=rota
 
+
+
 func _physics_process(delta: float):
 	velocity=Vector2(speed, 0).rotated(dir)
 	var collision_info = move_and_collide(velocity * delta)
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,7 +7,7 @@ var player_path=preload("res://player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("enemy")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
